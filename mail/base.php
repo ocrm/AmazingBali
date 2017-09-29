b9e354c740fda16e1717836351b2c9a4f62500c4
fijj <fijj606@gmail.com>
2017-09-29 16:26:30 +0300

    update

--- mail/base.php
+++ mail/base.php
@@ -7,16 +7,10 @@ use yii\helpers\Url;
 
 ?>
 
-<p>Дата сообщения: <?= $model->date ?></p>
-<br />
+<p>Дата сообщения: <?= date("Y-m-d H:i:s"); ?></p>
 <p>Имя: <?= $model->name ?></p>
-<br />
 <p>Телефон: <?= $model->phone ?></p>
-<br />
 <p>Почта: <?= $model->email ?></p>
-<br />
 <p>Что интересует: <?= $model->subject ?></p>
-<br />
-<p>Дата тура: <?= $model->tour_date ?></p>
-<br />
+<p>Дата тура: <?= Yii::$app->formatter->asDate($model->tour_date, "php:d-m-Y") ?></p>
 <p>Текст сообщения: <?= $model->text ?></p>
