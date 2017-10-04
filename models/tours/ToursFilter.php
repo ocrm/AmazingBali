4f9d3ef8faec4a80b98055a7d645d175332b15f3
fijj <fijj606@gmail.com>
2017-10-04 16:24:11 +0300

    update

--- models/tours/ToursFilter.php
+++ models/tours/ToursFilter.php
@@ -76,7 +76,7 @@ class ToursFilter extends Tours
             
         }else{
             
-            $query->orderBy(['sort' => SORT_DE]);
+            $query->orderBy(['sort' => SORT_DESC]);
             
         }
 
